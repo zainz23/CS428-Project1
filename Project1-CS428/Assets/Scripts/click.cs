46b61c630276a2d016a4cfde30f46c5ca44b939f
zzahra2 <zzahra2@uic.edu>
2019-09-21 15:42:21 -0500

    Uploads project alpha; Updates .gitignore; Fixes push error

---
+++ Project1-CS428/Assets/Scripts/click.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+
+public class click : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    public GameObject model;
+    public GameObject vb;
+    public AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vb.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+        model.SetActive(false);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        model.SetActive(true);
+        audio.Play();
+    }
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        audio.Stop();
+    }
+}
